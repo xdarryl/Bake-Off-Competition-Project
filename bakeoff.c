babc57135c32df981246efc72e9942ea4a3e69ae
Darryl <darrylo2020@gmail.com>
2024-11-12 22:56:31 -0500

    Basic file set up

---
+++ bakeoff.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *runner(void *param);
+
+int main(int argc, char* argv[])
+{
+	
+	int total_bakers;
+	
+	printf("Welcome to the 2024 CS452 Bake Off!\n");
+	printf("Enter the number of bakers for competition: \n");
+	
+	scanf("%d", &total_bakers);	
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
